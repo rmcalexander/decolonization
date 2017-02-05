6a8dd147ee005f62d09b0df56f5a45b0192a1796
rmcalexander <richardmcalexander@gmail.com>
2017-02-05 17:16:51 -0500

    Added code to load data

--- make_data.R
+++ make_data.R
@@ -1,1 +1,8 @@
+#LOAD PACKAGES
 library(tidyverse)
+library(haven)
+library(readr)
+
+#LOAD DATA
+WimmerFeinsteinReplication <- read_dta("~/Google Drive/R Projects/decolonization/WimmerFeinsteinReplication.dta")
+navco <- read_csv("~/Google Drive/R Projects/decolonization/navco.csv")
