64a862d077f2c3df8b363aae4814dfbdf8e84fa9
rmcalexander <richardmcalexander@gmail.com>
2017-02-27 10:50:13 -0500

    Completely revamped data, collected data on population level attributes, and created IV models

--- make_data.R
+++ make_data.R
@@ -22,7 +22,7 @@ navco<-filter(navco,target=="Soviet occupation"|
                 target=="British occupation"|
                 target=="UK occupation"|
                 target=="Portuguese occupation"|
-                target=="Dutch occpuation"|
+                target=="Dutch occupation"|
                 target=="French/Spanish occupation"|
                 target=="Belgian occupation"|
                 target=="British and Aden administration"
@@ -43,19 +43,32 @@ navco <- select(navco,-cowcode,-tccode)
 #merge
 decolonization<-merge(WimmerFeinsteinReplication,navco,all.x=TRUE)
 
+#correct Indonesia
+decolonization$Netherlands[decolonization$location=="Indonesia"]<-1
+decolonization$ns5emp[decolonization$location=="Indonesia" & decolonization$year==1949] <- 1
+
 #only include indepndent countries or countries that were part of european colonialism
 decolonization<-filter(decolonization,Netherlands==1|Portugal==1|Spain==1|indepstate==1|French==1|British==1) %>% 
   select(-Yugoslavia,-Ottoman,-KKAustria,-SU,-Romanov)
 
 #remove countries that were independent in 1945
 independence_vector <- summarise(group_by(decolonization,location),always_ind=min(indepstate))
-decolonization <- merge(decolonization,independence_vector) %>% 
-  filter(always_ind==0) 
+decolonization <- merge(decolonization,independence_vector) 
+decolonization$always_ind[decolonization$location=="Indonesia"]<-0
+decolonization<-filter(decolonization,always_ind==0) 
+
+test<-filter(decolonization,location=="Indonesia")
 
 rm(independence_vector)
 
+
 #remove all observations after a country receives independence
 decolonization <- decolonization[!is.na(decolonization$ns5emp),]
+
+#correct Indonesia
+decolonization$autonNS[decolonization$location=="Indonesia" & decolonization$year==1949]<-1
+decolonization$autonNS[decolonization$location=="Indonesia" & decolonization$year==1950]<-100
+decolonization<-filter(decolonization,autonNS!=100)
 
 #create vectors indicating if a resistance movement was active
 decolonization$any_resist <- as.numeric(!is.na(decolonization$resis_meth))
@@ -78,6 +91,8 @@ decolonization$ab_inter_reper[is.na(decolonization$ab_inter_reper)] <- 0
 decolonization$regime_support[decolonization$regime_support==-99]<-"NA"
 colnames(decolonization)[1]<-"country"
 
+decolonization$camp_conf_intensity[is.na(decolonization$camp_conf_intensity)]<-0
+
 #match with Easterly and Levine's settler population data
 easterly_levine_pop <- read_csv("~/Google Drive/R Projects/decolonization/easterly_levine_pop.csv", 
                                 col_types = cols(Blacks = col_integer(), 
@@ -89,6 +104,8 @@ easterly_levine_pop<-select(easterly_levine_pop,Country,Year,Whites,Euroshare) %
 easterly_levine_pop<-summarise(group_by(easterly_levine_pop,Country,Year),whites=mean(na.omit(Whites)),euroshare=mean(na.omit(Euroshare)))
 
 colnames(easterly_levine_pop)[1:2]<-c("country","year")
+
+decolonization <- merge(decolonization,easterly_levine_pop[,-3],all.x=TRUE)
 
 easterly_levine_pop<-(summarise(group_by(easterly_levine_pop,country),settler_pop=sum(na.omit(whites))))
 
@@ -124,4 +141,104 @@ decolonization$autonNS[decolonization$country=="Libya" & decolonization$year=="1
 decolonization <- cbind(as.numeric(decolonization$country=="Libya" & decolonization$year>1951),decolonization)
 colnames(decolonization)[1]<-"libyafilter"
 decolonization<-filter(decolonization,libyafilter==0) %>% select(-libyafilter)
+
+#fix Algeria
+decolonization$autonNS[decolonization$country=="Algeria" & decolonization$year=="1962"]<-1
+decolonization <- cbind(as.numeric(decolonization$country=="Algeria" & decolonization$year==1963),decolonization)
+colnames(decolonization)[1]<-"algeriafilter"
+decolonization<-filter(decolonization,algeriafilter==0) %>% select(-algeriafilter)
+
+#fix yemen
+decolonization$autonNS[decolonization$country=="Yemen" & decolonization$year=="1962"] <- 0
+
+
+#create dummy variable controlling for if the resistance switched from nonviolent to violent
+unique(select(filter(decolonization,violent_resist==1),country))
+unique(select(filter(decolonization,nonviolent_resist==1),country))
+
+decolonization$switched_from_non_to_violent<-as.numeric(decolonization$country=="Algeria"|
+                                                          decolonization$country=="Mozambique"|
+                                                          decolonization$country=="Tunisia"|
+                                                          decolonization$country=="Cyprus")
+decolonization$switched_from_violent_to_non<-as.numeric(decolonization$country=="Cameroon"|
+                                                          decolonization$country=="Cyprus")
+
+#lag method of resistance
+
+decolonization<-decolonization %>% group_by(country) %>% mutate(violent_resist.l1 = lag(violent_resist,n=1, order_by =year))
+decolonization<-decolonization %>% group_by(country) %>% mutate(nonviolent_resist.l1 = lag(nonviolent_resist,n=1, order_by =year))
+decolonization<-decolonization %>% group_by(country) %>% mutate(violent_resist.l2 = lag(violent_resist,n=2, order_by =year))
+decolonization<-decolonization %>% group_by(country) %>% mutate(nonviolent_resist.l2 = lag(nonviolent_resist,n=2, order_by =year))
+
+
+
+#include pop
+library(readxl)
+WPP2015_POP_F09_2_PERCENTAGE_OF_TOTAL_POPULATION_BY_BROAD_AGE_GROUP_MALE <- read_excel("~/Google Drive/R Projects/decolonization/WPP2015_POP_F09_2_PERCENTAGE_OF_TOTAL_POPULATION_BY_BROAD_AGE_GROUP_MALE.xls", 
+                                                                                       col_types = c("blank", "text", "text", 
+                                                                                                     "numeric", "numeric", "numeric", 
+                                                                                                     "numeric", "numeric", "numeric", 
+                                                                                                     "numeric", "numeric", "numeric", 
+                                                                                                     "numeric", "numeric", "numeric", 
+                                                                                                     "numeric", "numeric", "numeric", 
+                                                                                                     "numeric", "numeric", "numeric", 
+                                                                                                     "numeric", "numeric", "numeric", 
+                                                                                                     "numeric", "numeric", "numeric", 
+                                                                                                     "numeric", "numeric", "numeric", 
+                                                                                                     "numeric", "numeric", "numeric", 
+                                                                                                     "numeric", "numeric", "numeric", 
+                                                                                                     "numeric", "numeric", "numeric", 
+                                                                                                     "numeric", "numeric", "numeric", 
+                                                                                                     "numeric", "numeric", "numeric", 
+                                                                                                     "numeric", "numeric", "numeric", 
+                                                                                                     "numeric", "numeric", "numeric", 
+                                                                                                     "numeric", "numeric", "numeric", 
+                                                                                                     "numeric", "numeric", "numeric", 
+                                                                                                     "numeric", "numeric", "numeric", 
+                                                                                                     "text", "text"))
+
+decolonization$cown<-as.numeric(countrycode::countrycode(decolonization$country,"country.name","cown"))
+
+WPP2015_POP_F09_2_PERCENTAGE_OF_TOTAL_POPULATION_BY_BROAD_AGE_GROUP_MALE$cown<-as.numeric(countrycode::countrycode(WPP2015_POP_F09_2_PERCENTAGE_OF_TOTAL_POPULATION_BY_BROAD_AGE_GROUP_MALE$`Country code`,"iso3n","cown"))
+colnames(WPP2015_POP_F09_2_PERCENTAGE_OF_TOTAL_POPULATION_BY_BROAD_AGE_GROUP_MALE)[4]<-"year"
+WPP2015_POP_F09_2_PERCENTAGE_OF_TOTAL_POPULATION_BY_BROAD_AGE_GROUP_MALE$year<-as.integer(WPP2015_POP_F09_2_PERCENTAGE_OF_TOTAL_POPULATION_BY_BROAD_AGE_GROUP_MALE$year)
+
+decolonization<-merge(decolonization,WPP2015_POP_F09_2_PERCENTAGE_OF_TOTAL_POPULATION_BY_BROAD_AGE_GROUP_MALE,all.x=TRUE)
+
+#pop density
+
+WPP2015_POP_F06_POPULATION_DENSITY <- read_excel("~/Google Drive/R Projects/decolonization/WPP2015_POP_F06_POPULATION_DENSITY.xls")
+
+colnames(WPP2015_POP_F06_POPULATION_DENSITY)[1]<-"cown"
+WPP2015_POP_F06_POPULATION_DENSITY$cown<-as.numeric(countrycode::countrycode(WPP2015_POP_F06_POPULATION_DENSITY$`Country code`,"iso3n","cown"))
+WPP2015_POP_F06_POPULATION_DENSITY<-WPP2015_POP_F06_POPULATION_DENSITY[,-2]
+WPP2015_POP_F06_POPULATION_DENSITY<-na.omit(WPP2015_POP_F06_POPULATION_DENSITY)
+WPP2015_POP_F06_POPULATION_DENSITY$cown<-countrycode::countrycode(WPP2015_POP_F06_POPULATION_DENSITY$cown,"cown","country.name") 
+WPP2015_POP_F06_POPULATION_DENSITY$cown<-as.factor(WPP2015_POP_F06_POPULATION_DENSITY$cown)
+
+WPP2015_POP_F06_POPULATION_DENSITY<-reshape2::melt(WPP2015_POP_F06_POPULATION_DENSITY,ivar=cown)
+
+WPP2015_POP_F06_POPULATION_DENSITY$cown<-countrycode::countrycode(WPP2015_POP_F06_POPULATION_DENSITY$cown,"country.name","cown")
+
+colnames(WPP2015_POP_F06_POPULATION_DENSITY)<-c("cown","year","density")
+WPP2015_POP_F06_POPULATION_DENSITY$year<-as.numeric(as.character(WPP2015_POP_F06_POPULATION_DENSITY$year))
+
+decolonization<-merge(decolonization,WPP2015_POP_F06_POPULATION_DENSITY,all.x=TRUE)
+#WPP2015_POP_F06_POPULATION_DENSITY<-reshape2::melt(WPP2015_POP_F06_POPULATION_DENSITY)
+
+#interpolate
+library(zoo)
+decolonization<-decolonization[order(decolonization$country,decolonization$year),]
+
+decolonization<-decolonization %>% 
+ group_by(cown) %>% 
+  mutate(age_imputed=na.approx(`18-23`,rule=2))
+
+decolonization<-decolonization %>% 
+  group_by(cown) %>% 
+  mutate(density_imputed=na.approx(density,rule=2))
+
+decolonization<-decolonization %>% 
+  group_by(cown) %>% 
+  mutate(euroshare_imputed=na.locf.default(euroshare,na.rm=FALSE))
 
--- resistance_models.R
+++ resistance_models.R
@@ -1,7 +1,7 @@
 #main models
 summary(glm(autonNS ~
-              violent_resist+
-              csyear1+csyear2+rr_std+literacy+tot_ns+ns5emp+centercap+settler_pop+
+              violent_resist*camp_conf_intensity+ switched_from_violent_to_non+ switched_from_non_to_violent+
+              csyear1+csyear2+rr_std+literacy+tot_ns+ns5emp+nsf5neigh+centercap+settler_pop+
               Netherlands+Portugal+French+British+
               middleeast+africa+lamerica,
             family=binomial(link="probit"),data=decolonization))
@@ -9,7 +9,7 @@ summary(glm(autonNS ~
 
 summary(glm(autonNS ~
               nonviolent_resist+
-              csyear1+csyear2+rr_std+literacy+tot_ns+ns5emp+centercap+settler_pop+
+              csyear1+csyear2+rr_std+literacy+tot_ns+ns5emp+nsf5neigh+centercap+settler_pop+
               Netherlands+Portugal+French+British+
               middleeast+africa+lamerica,
             family=binomial(link="probit"),data=decolonization))
@@ -20,14 +20,14 @@ summary(glm(autonNS ~
 #media
 summary(glm(autonNS ~
              violent_resist*in_media+
-              csyear1+csyear2+rr_std+literacy+tot_ns+ns5emp+centercap+settler_pop+
+              csyear1+csyear2+rr_std+literacy+tot_ns+ns5emp+nsf5neigh+centercap+settler_pop+
               Netherlands+Portugal+French+British+
               middleeast+africa+lamerica,
             family=binomial(link="probit"),data=decolonization))
 #violence is less effective if people are paying attention
 summary(glm(autonNS ~
              nonviolent_resist*in_media+
-              csyear1+csyear2+rr_std+literacy+tot_ns+ns5emp+centercap+settler_pop+
+              csyear1+csyear2+rr_std+literacy+tot_ns+ns5emp+nsf5neigh+centercap+settler_pop+
               Netherlands+Portugal+French+British+
               middleeast+africa+lamerica,
             family=binomial(link="probit"),data=decolonization))
@@ -35,59 +35,88 @@ summary(glm(autonNS ~
 
 #does international condemnation matter?
 summary(glm(autonNS ~ violent_resist*ab_inter_con+
-              csyear1+csyear2+rr_std+literacy+tot_ns+ns5emp+centercap+settler_pop+
+              csyear1+csyear2+rr_std+literacy+tot_ns+ns5emp+nsf5neigh+centercap+settler_pop+
               Netherlands+Portugal+French+British+
               middleeast+africa+lamerica,
             family=binomial(link="probit"),data=decolonization))
 
 summary(glm(autonNS ~ nonviolent_resist*ab_inter_con+
-              csyear1+csyear2+rr_std+literacy+tot_ns+ns5emp+centercap+settler_pop+
+              csyear1+csyear2+rr_std+literacy+tot_ns+ns5emp+nsf5neigh+centercap+settler_pop+
               Netherlands+Portugal+French+British+
               middleeast+africa+lamerica,
             family=binomial(link="probit"),data=decolonization))
 
 summary(glm(autonNS ~ violent_resist+ab_inter_con+
-              csyear1+csyear2+rr_std+literacy+tot_ns+ns5emp+centercap+settler_pop+
+              csyear1+csyear2+rr_std+literacy+tot_ns+ns5emp+nsf5neigh+centercap+settler_pop+
               Netherlands+Portugal+French+British+
               middleeast+africa+lamerica,
             family=binomial(link="probit"),data=decolonization))
 
 summary(glm(autonNS ~ nonviolent_resist+ab_inter_con+
-              csyear1+csyear2+rr_std+literacy+tot_ns+ns5emp+centercap+settler_pop+
+              csyear1+csyear2+rr_std+literacy+tot_ns+ns5emp+nsf5neigh+centercap+settler_pop+
               Netherlands+Portugal+French+British+
               middleeast+africa+lamerica,
             family=binomial(link="probit"),data=decolonization))
 #not really
 
 
-#Bayes
-violent<-rstanarm::stan_glm(autonNS ~
-               violent_resist+
-               csyear1+csyear2+rr_std+literacy+tot_ns+ns5emp+centercap+settler_pop+
-               Netherlands+Portugal+French+British+
-               middleeast+africa+lamerica,
-             family=binomial,data=decolonization)
-
 
 #try survival models
 library(survival)
 S<-Surv(time=decolonization$year,event=decolonization$autonNS)
 summary(coxph(S~nonviolent_resist+ab_inter_con+rr_std+literacy+tot_ns+ns5emp+centercap+settler_pop+
-          Netherlands+Portugal+French+British+
+          Netherlands+Portugal+French+British+nsf5neigh+violent_resist.l2+
           middleeast+africa+lamerica,control = coxph.control(iter.max = 20),data=decolonization
          ))
 
 summary(coxph(S~violent_resist+ab_inter_con+rr_std+literacy+tot_ns+ns5emp+centercap+settler_pop+
-                Netherlands+Portugal+French+British+
+                Netherlands+Portugal+French+British+nsf5neigh+
                 middleeast+africa+lamerica,control = coxph.control(iter.max = 20),data=decolonization
 ))
 
 summary(coxph(S~nonviolent_resist*in_media+ab_inter_con+rr_std+literacy+tot_ns+ns5emp+centercap+settler_pop+
-                Netherlands+Portugal+French+British+
+                Netherlands+Portugal+French+British+nsf5neigh+
                 middleeast+africa+lamerica,control = coxph.control(iter.max = 20),data=decolonization
 ))
 
 summary(coxph(S~violent_resist*in_media+ab_inter_con+rr_std+literacy+tot_ns+ns5emp+centercap+settler_pop+
-                Netherlands+Portugal+French+British+
+                Netherlands+Portugal+French+British+nsf5neigh+
                 middleeast+africa+lamerica,control = coxph.control(iter.max = 20),data=decolonization
 ))
+
+summary(coxph(S~nonviolent_resist*camp_conf_intensity+ab_inter_con+rr_std+literacy+tot_ns+ns5emp+centercap+settler_pop+
+                Netherlands+Portugal+French+British+nsf5neigh+
+                middleeast+africa+lamerica,control = coxph.control(iter.max = 20),data=decolonization
+))
+
+summary(coxph(S~violent_resist*camp_conf_intensity+ab_inter_con+rr_std+literacy+tot_ns+ns5emp+centercap+settler_pop+
+                Netherlands+Portugal+French+British+nsf5neigh+
+                middleeast+africa+lamerica,control = coxph.control(iter.max = 20),data=decolonization
+))
+
+#try using population for instrument
+
+summary(AER::ivreg(
+  autonNS~violent_resist+csyear1+csyear2+rr_std+literacy+
+    Portugal+French+British+africa+asia+lamerica+settler_pop
+  |csyear1+csyear2+rr_std+literacy+
+    Portugal+French+British+africa+asia+lamerica+settler_pop
+  +age_imputed,data=decolonization
+),diagnostics = TRUE)
+
+summary(AER::ivreg(
+  autonNS~violent_resist+csyear1+csyear2+rr_std+literacy+
+    Portugal+French+British+africa+asia+lamerica+settler_pop
+  |csyear1+csyear2+rr_std+literacy+
+    Portugal+French+British+africa+asia+lamerica+settler_pop
+  +density_imputed,data=decolonization
+),diagnostics = TRUE)
+
+summary(AER::ivreg(
+  autonNS~violent_resist+csyear1+csyear2+rr_std+literacy+
+    Portugal+French+British+africa+asia+lamerica+settler_pop
+  |csyear1+csyear2+rr_std+literacy+
+    Portugal+French+British+africa+asia+lamerica+settler_pop
+  +age_imputed+density_imputed,data=decolonization
+),diagnostics = TRUE)
+
