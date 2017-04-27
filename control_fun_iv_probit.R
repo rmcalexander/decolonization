6359ed2a90aab824c8c33ebded0afb82d0132b7f
rmcalexander <richardmcalexander@gmail.com>
2017-04-27 11:30:23 -0400

    polished

--- control_fun_iv_probit.R
+++ control_fun_iv_probit.R
@@ -1,29 +1,35 @@
-library(SemiParBIVProbit)
+#this file uses a control function approach to account for endogeneity in violent and nonviolent resistance
+
+#create new dataset
+#only include countries that have ever experienced resistance to colonial rule.
+
 cf_probitdf<-ivprobitdf
 cf_probitdf<-dplyr::select(cf_probitdf,density_imputed,violent_resist,nonviolent_resist,time1,time2,French,British,africa,settler_pop,rr_std,AREA,literacy,autonNS,country) %>% na.omit()
 cf_probitdf$density_log<-log(cf_probitdf$density_imputed+1)
 
-
-summary(SemiParBIVProbit(list(
-  nonviolent_resist ~ density_log +violent_resist+ time1+time2+French+British+africa+settler_pop+rr_std+AREA+literacy,
-  autonNS ~ violent_resist +nonviolent_resist+ time1+time2+French+British+africa+settler_pop+rr_std+AREA+literacy),
-  data = ivprobitdf,fp=TRUE,margins=c("probit","probit")))
-
-summary(SemiParBIVProbit(list(
-  violent_resist ~ density_log +nonviolent_resist+ time1+time2+French+British+africa+settler_pop+rr_std+AREA+literacy,
-  autonNS ~ nonviolent_resist +violent_resist+ time1+time2+French+British+africa+settler_pop+rr_std+AREA+literacy),
-  data = ivprobitdf,fp=TRUE,margins=c("probit","probit")))
-
-residual<-
+#collect first stage probit residuals when violent resistance is the endogenous binary DV.
+#probit link
+residual_v<-
   residuals(glm(violent_resist ~ density_log +nonviolent_resist+ time1+time2+French+British+africa+settler_pop+rr_std+AREA+literacy,data=cf_probitdf,family = binomial(link = "probit")))/
   sd(residuals(glm(violent_resist ~ density_log +nonviolent_resist+ time1+time2+French+British+africa+settler_pop+rr_std+AREA+literacy,data=cf_probitdf,family = binomial(link = "probit"))))
 
-summary(glm(autonNS ~ violent_resist +nonviolent_resist+ time1+time2+French+British+africa+settler_pop+rr_std+AREA+literacy+residual,family = binomial(link = "probit"),data=cf_probitdf))
+#second stage
+#probit
+summary(glm(autonNS ~ violent_resist +nonviolent_resist+ time1+time2+French+British+africa+settler_pop+rr_std+AREA+literacy+residual_v,family = binomial(link = "probit"),data=cf_probitdf))
 
+#logit
+summary(glm(autonNS ~ violent_resist +nonviolent_resist+ time1+time2+French+British+africa+settler_pop+rr_std+AREA+literacy+residual_v,family = binomial(link = "logit"),data=cf_probitdf))
 
-cf_probitdf$residual<-
-  residuals(glm(violent_resist ~ density_log +nonviolent_resist+ time1+time2+as.factor(country),data=cf_probitdf,family = binomial(link = "probit")))/
-  sd(residuals(glm(violent_resist ~ density_log +nonviolent_resist+ time1+time2++as.factor(country),data=cf_probitdf,family = binomial(link = "probit"))))
+#now do same for nonviolent resistance
+#probit link
+residual_nv<-
+  residuals(glm(nonviolent_resist ~ density_log +violent_resist+ time1+time2+French+British+africa+settler_pop+rr_std+AREA+literacy,data=cf_probitdf,family = binomial(link = "probit")))/
+  sd(residuals(glm(nonviolent_resist ~ density_log +violent_resist+ time1+time2+French+British+africa+settler_pop+rr_std+AREA+literacy,data=cf_probitdf,family = binomial(link = "probit"))))
 
-summary(glm(autonNS ~ violent_resist +nonviolent_resist+ time1+time2+as.factor(country)+residual,family = binomial(link = "probit"),data=cf_probitdf))
+#second stage
+#probit
+summary(glm(autonNS ~ violent_resist +nonviolent_resist+ time1+time2+French+British+africa+settler_pop+rr_std+AREA+literacy+residual_nv,family = binomial(link = "probit"),data=cf_probitdf))
+
+#logit
+summary(glm(autonNS ~ violent_resist +nonviolent_resist+ time1+time2+French+British+africa+settler_pop+rr_std+AREA+literacy+residual_nv,family = binomial(link = "logit"),data=cf_probitdf))
 
